b7ec9dd055d04d8e744a68969f1d1a21c9a36530
Cicero Alves <cicerojprn@gmail.com>
2018-10-04 13:41:18 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/cicinho/peer/RegularPeerProjectApplication.java
@@ -0,0 +1,12 @@
+package com.cicinho.peer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegularPeerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegularPeerProjectApplication.class, args);
+	}
+}
