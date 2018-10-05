fba0cc7379169e3bf1b3ea7578d1b571206895ea
Cicero Alves <cicerojprn@gmail.com>
2018-10-05 15:44:40 -0300

    Modificação para utilizar o application.properties para configurar o nó

--- src/main/java/com/cicinho/peer/RegularPeerProjectApplication.java
+++ src/main/java/com/cicinho/peer/RegularPeerProjectApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.cicinho.peer.regular.PrivateNetworkRegularPeer;
 
 @SpringBootApplication
-public class RegularPeerProjectApplication {
-
+public class RegularPeerProjectApplication {	
+		
 	public static void main(String[] args) {
 		SpringApplication.run(RegularPeerProjectApplication.class, args);
-		
+	
 		PrivateNetworkRegularPeer.initRegularPeer();
 	}
 }
--- src/main/java/com/cicinho/peer/regular/PrivateNetworkRegularPeer.java
+++ src/main/java/com/cicinho/peer/regular/PrivateNetworkRegularPeer.java
@@ -1,9 +1,13 @@
 package com.cicinho.peer.regular;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.ethereum.config.SystemProperties;
 import org.ethereum.facade.EthereumFactory;
 import org.ethereum.samples.BasicSample;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import com.cicinho.peer.node.RegularNode;
 import com.typesafe.config.ConfigFactory;
@@ -11,26 +15,30 @@ import com.typesafe.config.ConfigFactory;
 public class PrivateNetworkRegularPeer {
 
 	private static String logger = "RegularPeer1";
-	private static String externalIp = "198.168.1.5";
+	
+	static Properties properties;
 
 	/**
 	 * Spring configuration class for the Regular peer
 	 */
 	private static class RegularConfig {
+		
 		private final String config =
 				// no discovery: we are connecting directly to the miner peer
-				"peer.discovery.enabled = false \n" + "peer.listen.port = 30336 \n"
-						+ "peer.privateKey = 3ec771c31cac8c0dba77a69e503765701d3c2bb62435888d4ffa38fed60c445c \n"
-						+ "peer.networkId = 15 \n" +
+				"peer.discovery.enabled = " + properties.getProperty("peer.discovery.enabled") + " \n" + 
+						"peer.listen.port = " + properties.getProperty("peer.listen.port") + " \n"
+						+ "peer.privateKey = " + properties.getProperty("peer.privateKey") + " \n"
+						+ "peer.networkId = " + properties.getProperty("peer.networkId") + " \n" +
 						// actively connecting to the miner
 						"peer.active = ["
-						+ "    { url = 'enode://26ba1aadaf59d7607ad7f437146927d79e80312f026cfa635c6b2ccf2c5d3521f5812ca2beb3b295b14f97110e6448c1c7ff68f14c5328d43a3c62b44143e9b1@localhost:30335' }"
-						+ "] \n" + "sync.enabled = true \n" +
+						+ "    { url = '" + properties.getProperty("enode.miner1") + "' }"
+						+ "] \n" 
+						+ "sync.enabled = " + properties.getProperty("sync.enabled") + " \n" +
 						// all peers in the same network need to use the same genesis block
-						"genesis = genesis.json \n" +
+						"genesis = " + properties.getProperty("genesis") + " \n" +
 						// two peers need to have separate database dirs
-						"database.dir = database \n"
-						+ "peer.discovery.external.ip = " + externalIp;;
+						"database.dir = " + properties.getProperty("database.dir") + " \n"
+						+ "peer.discovery.external.ip = " + properties.getProperty("peer.discovery.external.ip");
 
 		@Bean
 		public RegularNode node() {
@@ -51,6 +59,10 @@ public class PrivateNetworkRegularPeer {
 	}
 
 	public static void initRegularPeer() {
+		try {
+			properties = PropertiesLoaderUtils.loadAllProperties("application.properties");
+		} catch (IOException e) {			
+		}
 		BasicSample.sLogger.info("Starting EthtereumJ regular instance!");
 		EthereumFactory.createEthereum(RegularConfig.class);
 	}
