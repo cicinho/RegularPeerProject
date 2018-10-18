ab48b1c4ce57c65fcb16bce3573157fea6faad66
Cicero Alves <cicerojprn@gmail.com>
2018-10-18 15:13:00 -0300

    Comentários com as chaves privadas e públicas para serem utilizadas em testes

--- src/main/java/com/cicinho/peer/node/RegularNode.java
+++ src/main/java/com/cicinho/peer/node/RegularNode.java
@@ -60,7 +60,7 @@ public class RegularNode extends BasicSample {
 
 		// the sender which some coins from the genesis
 		ECKey senderKey = ECKey
-				.fromPrivate(Hex.decode("6ef8da380c27cea8fdf7448340ea99e8e2268fc2950d79ed47cbf6f85dc977ec"));
+				.fromPrivate(Hex.decode("3ec771c31cac8c0dba77a69e503765701d3c2bb62435888d4ffa38fed60c445c"));
 		byte[] receiverAddr = Hex.decode("5db10750e8caff27f906b41c71b3471057dd2004");
 
 		for (int i = ethereum.getRepository().getNonce(senderKey.getAddress()).intValue(), j = 0; j < 20000; i++, j++) {
@@ -79,11 +79,29 @@ public class RegularNode extends BasicSample {
 	private void generateOneTransaction() throws Exception {
 		logger.info("Start generating a transaction...");
 
+		//PrivateKey
+		//3ec771c31cac8c0dba77a69e503765701d3c2bb62435888d4ffa38fed60c445c
+		//PublicKey
+		//5db10750e8caff27f906b41c71b3471057dd2004
+		
+		//PrivateKey
+		//6ef8da380c27cea8fdf7448340ea99e8e2268fc2950d79ed47cbf6f85dc977ec
+		//PublicKey
+		//31e2e1ed11951c7091dfba62cd4b7145e947219c
+		
+		//PrivateKey
+		//fee3b6045d75237490f1ba055bf6d034b2a83c71c78fb526b3183b5c68944f1d
+		//PublicKey
+		//ee0250c19ad59305b2bdb61f34b45b72fe37154f
+		
 		// the sender which some coins from the genesis
 		ECKey senderKey = ECKey
 				.fromPrivate(Hex.decode("6ef8da380c27cea8fdf7448340ea99e8e2268fc2950d79ed47cbf6f85dc977ec"));
-		byte[] receiverAddr = Hex.decode("5db10750e8caff27f906b41c71b3471057dd2004");
+		byte[] receiverAddr = Hex.decode("31e2e1ed11951c7091dfba62cd4b7145e947219c");
 
+		System.out.println("SENDER: " + ByteUtil.toHexString(senderKey.getAddress()));
+		System.out.println("RECEIVER: " + ByteUtil.toHexString(receiverAddr));
+		
 		int i = ethereum.getRepository().getNonce(senderKey.getAddress()).intValue();
 		Transaction tx = new Transaction(ByteUtil.intToBytesNoLeadZeroes(i),
 				ByteUtil.longToBytesNoLeadZeroes(50_000_000_000L), ByteUtil.longToBytesNoLeadZeroes(0xfffff),
@@ -91,6 +109,5 @@ public class RegularNode extends BasicSample {
 		tx.sign(senderKey);
 		logger.info("<== Submitting tx: " + tx);
 		ethereum.submitTransaction(tx);
-
 	}
 }
