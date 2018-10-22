bec0e6b31844d35481c87f83854ed055c451f812
cicero <cicerojprn@gmail.com>
2018-10-22 09:12:08 -0300

    Indentificação dos endereços de SENDER e RECEIVER em atributos da classe

--- src/main/java/com/cicinho/peer/node/RegularNode.java
+++ src/main/java/com/cicinho/peer/node/RegularNode.java
@@ -10,6 +10,24 @@ import org.spongycastle.util.encoders.Hex;
 
 public class RegularNode extends BasicSample {
 
+	// PrivateKey
+	// 3ec771c31cac8c0dba77a69e503765701d3c2bb62435888d4ffa38fed60c445c
+	// PublicKey
+	// 5db10750e8caff27f906b41c71b3471057dd2004
+
+	// PrivateKey
+	// 6ef8da380c27cea8fdf7448340ea99e8e2268fc2950d79ed47cbf6f85dc977ec
+	// PublicKey
+	// 31e2e1ed11951c7091dfba62cd4b7145e947219c
+
+	// PrivateKey
+	// fee3b6045d75237490f1ba055bf6d034b2a83c71c78fb526b3183b5c68944f1d
+	// PublicKey
+	// ee0250c19ad59305b2bdb61f34b45b72fe37154f
+	
+	private String senderPrivateAddress = "6ef8da380c27cea8fdf7448340ea99e8e2268fc2950d79ed47cbf6f85dc977ec";
+	private String receiverPublicAdrress = "31e2e1ed11951c7091dfba62cd4b7145e947219c";
+
 	public RegularNode(String logger) {
 		// peers need different loggers
 		super(logger);
@@ -26,7 +44,7 @@ public class RegularNode extends BasicSample {
 			Scanner scanner = new Scanner(System.in);
 			int option, nonce;
 			ECKey senderKey = ECKey
-					.fromPrivate(Hex.decode("6ef8da380c27cea8fdf7448340ea99e8e2268fc2950d79ed47cbf6f85dc977ec"));
+					.fromPrivate(Hex.decode(senderPrivateAddress));
 			nonce = ethereum.getRepository().getNonce(senderKey.getAddress()).intValue();
 			do {
 				System.out.println("MENU");
@@ -67,8 +85,8 @@ public class RegularNode extends BasicSample {
 
 		// the sender which some coins from the genesis
 		ECKey senderKey = ECKey
-				.fromPrivate(Hex.decode("3ec771c31cac8c0dba77a69e503765701d3c2bb62435888d4ffa38fed60c445c"));
-		byte[] receiverAddr = Hex.decode("5db10750e8caff27f906b41c71b3471057dd2004");
+				.fromPrivate(Hex.decode(senderPrivateAddress));
+		byte[] receiverAddr = Hex.decode(receiverPublicAdrress);
 
 		for (int i = ethereum.getRepository().getNonce(senderKey.getAddress()).intValue(), j = 0; j < 20000; i++, j++) {
 			{
@@ -86,25 +104,10 @@ public class RegularNode extends BasicSample {
 	private void generateOneTransaction(int nonce) throws Exception {
 		logger.info("Start generating a transaction...");
 
-		// PrivateKey
-		// 3ec771c31cac8c0dba77a69e503765701d3c2bb62435888d4ffa38fed60c445c
-		// PublicKey
-		// 5db10750e8caff27f906b41c71b3471057dd2004
-
-		// PrivateKey
-		// 6ef8da380c27cea8fdf7448340ea99e8e2268fc2950d79ed47cbf6f85dc977ec
-		// PublicKey
-		// 31e2e1ed11951c7091dfba62cd4b7145e947219c
-
-		// PrivateKey
-		// fee3b6045d75237490f1ba055bf6d034b2a83c71c78fb526b3183b5c68944f1d
-		// PublicKey
-		// ee0250c19ad59305b2bdb61f34b45b72fe37154f
-
 		// the sender which some coins from the genesis
 		ECKey senderKey = ECKey
-				.fromPrivate(Hex.decode("6ef8da380c27cea8fdf7448340ea99e8e2268fc2950d79ed47cbf6f85dc977ec"));
-		byte[] receiverAddr = Hex.decode("31e2e1ed11951c7091dfba62cd4b7145e947219c");
+				.fromPrivate(Hex.decode(senderPrivateAddress));
+		byte[] receiverAddr = Hex.decode(receiverPublicAdrress);
 
 		Transaction tx = new Transaction(ByteUtil.intToBytesNoLeadZeroes(nonce),
 				ByteUtil.longToBytesNoLeadZeroes(50_000_000_000L), ByteUtil.longToBytesNoLeadZeroes(0xfffff),
