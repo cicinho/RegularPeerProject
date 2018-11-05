34b39e89a1ed6d3ffb56bdd5e0c1ebd35ad564cf
Cicero Alves <cicerojprn@gmail.com>
2018-11-05 12:59:06 -0300

    Adição da chave privada na classe NodeWallet

--- src/main/java/com/cicinho/peer/wallet/NodeWallet.java
+++ src/main/java/com/cicinho/peer/wallet/NodeWallet.java
@@ -4,20 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.ethereum.core.Transaction;
-import org.ethereum.core.TransactionReceipt;
+import org.ethereum.crypto.ECKey;
+import org.ethereum.util.ByteUtil;
+import org.spongycastle.util.encoders.Hex;
 
 public class NodeWallet {
 
+	private String privateKey;
+	
 	private List<Transaction> sentTransactions;
 	
 	private List<Transaction> receivedTransactions;
 	
-	private List<TransactionReceipt> sentTransactionReceipts;
-	
-	public NodeWallet() {
+	public NodeWallet(String privateKey) {
 		this.sentTransactions = new ArrayList<>();
 		this.receivedTransactions = new ArrayList<>();
-		this.sentTransactionReceipts = new ArrayList<>();
+		this.privateKey = privateKey;
+	}		
+
+	public String getPrivateKey() {
+		return privateKey;
+	}
+	
+	public String getPublicKey () {		
+		return ByteUtil.toHexString(ECKey.fromPrivate(Hex.decode(getPrivateKey())).getAddress());
+	}
+
+	public void setPrivateKey(String privateKey) {
+		this.privateKey = privateKey;
 	}
 
 	public List<Transaction> getSentTransactions() {
@@ -27,9 +41,4 @@ public class NodeWallet {
 	public List<Transaction> getReceivedTransactions() {
 		return receivedTransactions;
 	}
-
-	public List<TransactionReceipt> getSentTransactionReceipts() {
-		return sentTransactionReceipts;
-	}	
-	
 }
